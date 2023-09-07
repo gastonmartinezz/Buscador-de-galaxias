adacc511163c21e1779974af09fa897d87e0717f
gastonmartinezz <ezegaston2011@gmail.com>
2023-09-06 21:17:38 -0300

    casi finalizado el js, css y html

--- js/space.js
+++ js/space.js
@@ -1,11 +1,42 @@
-const api = "https://images-api.nasa.gov/";
+let valor = document.getElementById('inputBuscar');
 
-async function getJSONdata() {
-    try {
-        const response = await fetch(api);
-        const galaxia = await response.json();
-        console.log("El fetch se hizo bien");
-    } catch(error) {    
-        console.log("No se hizo bien el fetch.")
+let btn = document.getElementById('btnBuscar');
+
+btn.addEventListener('click', () => {
+    const api = `https://images-api.nasa.gov/search?q=${valor.value}`;
+    fetch(api)
+    .then(response => response.json())
+    .then(data => crearFichas(data));
+
+    function crearFichas(data) {
+        const conte = document.getElementById('contenedor');
+        conte.innerHTML = '';
+
+        for (let i of data.collection.items) {
+            let title = i.data[0].title;
+            let desc = i.data[0].description;
+            let fecha = i.data[0].date_created;
+            let img = i.links[0].href;
+
+
+            conte.innerHTML += 
+            `
+            <div class="fichas">
+                <div class="imagen">
+                    <img src= ${img}></img>
+                </div>
+                <div class="descDeFicha">
+                    <p class="titulo"> ${title} </p>
+                    <p class ="descripcion"> ${desc} </p>
+                    <p class ="fechas"> ${fecha}</p>
+                </div>
+            <div>
+          `
+        }
     }
-}
+})
+
+
+
+
+
